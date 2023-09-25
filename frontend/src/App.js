2c1bea3876d2471c5f7a87621340176240a7c365
Indrajit Pal <palindrajit10@gmail.com>
2023-09-25 14:47:18 +0530

    footer updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,17 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
 import Search from "./Pages/Search/Search";
-
+import Footer from "./components/Navbar/Footer/Footer";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path='/flights/search' element={<Search/>} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path='/flights/search' element={<Search />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ frontend/src/components/Navbar/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../Footer/Footer.css"
+
+const Footer = () => {
+    return (
+        <>
+            <footer class="page-footer font-small blue footer fixed-bottom">
+                <div class="footer-copyright text-center py-3">© 2023 Copyright:
+                    <a>flightheaven.com</a>
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
